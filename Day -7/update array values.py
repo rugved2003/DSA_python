c53ae444b0dc195aa83b9a5504d377e2f3e03309
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-21 14:13:53 +0530

    Update update array values.py

--- Day -7/update array values.py
+++ Day -7/update array values.py
@@ -3,6 +3,6 @@ l = list(map(int,input("Enter the elements:").strip().split()))[:n]
 print(l)
 p = int(input("postion:"))
 new_e = int(input("new element:"))
-l.remove(p+1)
-l.insert(p,new_e)
+l.remove(p)
+l.insert(p-1,new_e)
 print(l)
