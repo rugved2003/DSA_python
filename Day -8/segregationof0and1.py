09849cebe5f6170bf402b0a9cefd9cdcfd258f04
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-22 14:49:26 +0530

    Create segregationof0and1.py

---
+++ Day -8/segregationof0and1.py
@@ -0,0 +1,21 @@
+def segregate0and1(arr):
+    n = len(arr)
+    count = 0
+    
+    for i in range(n):
+        if arr[i] == 0:
+            count += 1
+            
+    for i in range(count):
+        arr[i] = 0
+    for i in range(count,n):
+        arr[i] = 1
+
+def print_arr(arr):
+    print("Array after segregation is ",end = " ")
+    for i in range(len(arr)):
+        print(arr[i],end = ' ')
+
+arr = [0,1,0,1,1,0,0,1]
+segregate0and1(arr)
+print_arr(arr)
