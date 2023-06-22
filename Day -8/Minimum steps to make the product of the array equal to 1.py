2d380c610ee12762fa08e57fc0198bc6991fbaae
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-22 14:03:28 +0530

    Create Minimum steps to make the product of the array equal to 1.py

---
+++ Day -8/Minimum steps to make the product of the array equal to 1.py
@@ -0,0 +1,31 @@
+def Minstep(a,n):
+    pos = 0
+    neg = 0
+    zero = 0
+    
+    step = 0
+    
+    for i in range(n):
+        if a[i] == 0:
+            zero += 1
+            
+        elif a[i]<0:
+            neg += 1
+            step = step + (-1-a[i])
+            
+        else:
+            pos +=1
+            step = step + (a[i] - 1)
+            
+    if neg % 2 == 0:
+        step = step +zero
+    else:
+        if zero >0:
+            step = step +zero
+        else:
+            step = step +2
+    return step 
+    
+a = [0,-2,4]
+n = len(a)
+print(Minstep(a,n))
