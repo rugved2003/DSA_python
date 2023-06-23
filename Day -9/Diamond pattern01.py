7777f94f443b52b4d53c4e96726771abea6e40d6
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-23 13:48:18 +0530

    Create Diamond pattern01.py

---
+++ Day -9/Diamond pattern01.py
@@ -0,0 +1,20 @@
+n = 5
+for i in range(n):
+    x = 1
+    for j in range(2*n-1):
+        if(i+j<n-1 or i+j >= n+2*i):
+            print(" ",end = "")
+        else:
+            print(x,end = "")
+            x += 1
+    print()
+    
+for i in range(n-1):
+    x =1
+    for j in range(2*n-1):
+        if(i+j<= 2*i or i+j>=(2*n-2)):
+            print(" ",end = "")
+        else:
+            print(x,end = "")
+            x+=1
+    print()
