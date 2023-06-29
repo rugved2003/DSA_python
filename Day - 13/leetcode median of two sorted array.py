9c2ff86cd1fbfb2a3363d1118b578e812af97c05
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-29 14:31:27 +0530

    Delete Day - 13 directory

--- Day - 13/leetcode median of two sorted array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        arr3: List[int] = nums1 + nums2
-        arr3.sort()
-        len3: int = len(arr3)
-        return (arr3[len3//2-1]/2.0 + arr3[len3//2] / 2.0, arr3[len3//2])[len3 %2] if len3 else None
